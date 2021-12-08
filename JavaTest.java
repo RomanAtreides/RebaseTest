9c67d47e29795d8ccddc5493050ee3e7cbb7299c
RomanAtreides <justaspeckinspace@gmail.com>
2021-12-08 21:35:01 +0300

    Add two new files JavaTest.java and JavaTest.class

---
+++ JavaTest.java
@@ -0,0 +1,10 @@
+public class JavaTest {
+    public static void main(String[] args) {
+        String s = " ";
+        char space = ' ';
+        int hex = 0xFF;
+        int oct = 011;
+        int bin = 0b1100;
+        System.out.println(hex + "\n" + oct + "\n" + bin);
+    }
+}
