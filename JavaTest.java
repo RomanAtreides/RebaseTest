02fca5cfa4fa9417d537ade9558892a8f6f398d0
RomanAtreides <justaspeckinspace@gmail.com>
2021-12-10 15:31:16 +0300

    trying to fix merge conflicts

--- JavaTest.java
+++ JavaTest.java
@@ -10,7 +10,10 @@ public class JavaTest {
         System.out.println("Представление чисел в различных системах счисления\n");
         System.out.println("В шестнадцатеричной\n\tв восьмеричной\n\t\tв двоичной");
         System.out.println("0xFF\n" + hex + "\n\t011\n\t" + oct + "\n\t\t0b1100\n\t\t" + bin);
+<<<<<<< HEAD
 
         if(hex > oct) System.out.println(hex + " is bigger then " + oct);
+=======
+>>>>>>> da66f66 (4 commit on second-feature - delete string Hello from JavaTest.java file)
     }
 }
